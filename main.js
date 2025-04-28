175ef313029d786407b329913fea3d810ae7bf45
inexorableace <rcsaunder@gmail.com>
2025-04-27 22:33:13 -0400

    new track and font

--- main.js
+++ main.js
@@ -294,10 +294,14 @@ var sData = {
     74:{
         "name": "Rain from the Ground",
         "game": "Clair Obscur: Expedition 33"
+    },
+    75:{
+        "name": "Redial HD",
+        "game": "Bomberman Hero"
     }
 }
 
-var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74];
+var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75];
 var gen_nums = [];
 
 function in_array(array, el) {
@@ -336,8 +340,7 @@ function calculateCurrentValue(currentTime) {
 	const currentSecondsLong = currentTime % 60;
 	const currentSeconds = currentSecondsLong.toFixed();
 	const currentTimeFormatted = `${currentMinute < 10 ? `0${currentMinute}` : currentMinute}:${
-		currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds
-	}`;
+		currentSeconds < 10 ? `0${currentSeconds}` : currentSeconds}`;
 	return currentTimeFormatted;
 }
 
@@ -376,7 +379,7 @@ function playlist() {
 
 var firstTrackNum = get_rand(nums);
 var nextSong = 'tracks/' + firstTrackNum + '.mp3';
-trackInfo.innerHTML = `<b>${sData[firstTrackNum].name}</b> : ${sData[firstTrackNum].game}`
+trackInfo.innerHTML = `${sData[firstTrackNum].name} - ${sData[firstTrackNum].game}`
 console.log(nextSong)
 audioPlayer.src = nextSong;
 
