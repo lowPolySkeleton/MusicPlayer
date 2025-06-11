8076e507ad153846cb7a603d679434c49e2a141e
inexorableace <rcsaunder@gmail.com>
2025-06-11 17:58:47 -0400

    remove redundant array

--- main.js
+++ main.js
@@ -477,8 +477,8 @@ var trackMetadata = {
     },
 }
 
-var trackIDs = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,76,77,78,79,80,81,82,83,84,85,86,87,88,89,90,91,92,93,94,95];
-
+// create array from tracks
+var trackIDs = Object.entries(trackMetadata).map(([key]) => key);
 
 const audioPlayer = document.getElementById('audioPlayer');
 const uiTrackInfo = document.getElementById('trackInfo');
@@ -508,11 +508,9 @@ function shuffle(array) {
 }
 
 
-
 function initRandomPlaylist(){
     //shuffle the trackIDs
     shuffle(trackIDs);
-    console.log(trackIDs)
     const currentTrack = trackIDs[currentTrackIndex];
     const songName = trackMetadata[currentTrack].name;
     const songGame = trackMetadata[currentTrack].game;
@@ -666,7 +664,7 @@ client.on('message', (wat, tags, message, self) => {
         initPreviousTrack();
     }
 
-    if((message.startsWith("!play") || message.startsWith("!pause"))&& tags.username == "lowpolyskeleton"){
+    if((message.startsWith("!play") || message.startsWith("!pause")) && tags.username == "lowpolyskeleton"){
         playPause();
     }
 
